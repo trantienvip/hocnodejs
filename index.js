9cf721e1820f16f7da6ea027215c3c99d46b38ac
Tran tien <trantien120498@gmail.com>
2021-08-24 17:19:39 +0700

    bootrap them tien

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000
- 
-app.get('/', function (req, res) {
-  res.send('Hello World đã thay đổi định')
-})
- 
-app.listen(port)
