a04325ded0daa7402020655e9eb97408db5db368
Tran tien <trantien120498@gmail.com>
2021-08-24 10:04:10 +0700

    dinh thay doi

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000
  
 app.get('/', function (req, res) {
-  res.send('Hello World đã thay đổi')
+  res.send('Hello World đã thay đổi định')
 })
  
 app.listen(port)
