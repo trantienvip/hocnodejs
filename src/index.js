209450d2ca276c847bec84cb07d09bd1b2775fa5
Tran tien <trantien120498@gmail.com>
2021-08-25 17:29:43 +0700

    tien da thay doi ngay 25 thang 8

--- src/index.js
+++ src/index.js
@@ -2,24 +2,27 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const morgan = require('morgan');
-const handlebars  = require('express-handlebars');
-const port = 3000
+const handlebars = require('express-handlebars');
+const port = 3000;
+
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 // HTTP LOGGER
 // app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', handlebars({extname: '.hbs'}));
-app.set('view engine', 'hbs'); 
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
-app.get('/', function (req, res) {
-  res.render('home');
-})
-app.get('/news', function (req, res) {
-  res.render('news');
-})
- 
-app.listen(port)
+//Routes init
+route(app);
+
+app.listen(port);
